56d667b5e9a32050dfee7190064136df1cc46dd7
deana <dvandegriff1@gmail.com>
2022-06-09 11:52:36 -0500

    adds reverse array

--- reverse_array.rb
+++ reverse_array.rb
@@ -3,5 +3,14 @@
 # Input: [1, 2, 3, 4, 5]
 # Output: [5, 4, 3, 2, 1]
 
-def reverse_array
+def reverse_array(nums)
+  index = -1
+  reverse_nums = []
+  nums.length.times do
+    reverse_nums << nums[index]
+    index = index + -1
+  end
+  return reverse_nums
 end
+
+p reverse_array([1, 2, 3, 4, 5])
